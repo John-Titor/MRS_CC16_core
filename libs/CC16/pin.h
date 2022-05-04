2fcd76422f930b01cfcf6bf144b688ee29e6a5fd
none <user@example.com>
2022-05-04 10:18:54 -0700

    More integrated (if slightly more verbose) GPIO configuration.

---
+++ libs/CC16/pin.h
@@ -0,0 +1,344 @@
+// Common pin and GPIO functionality.
+//
+
+#pragma once
+
+#include <stdint.h>
+#include <CMSIS/S32K144.h>
+
+namespace Pin
+{
+
+class _Pin
+{
+public:
+    constexpr _Pin(__IO void *cfgbase,
+                   uint8_t index) :
+        _cfgh   { ((PORTA_Type *)cfgbase)->PORTA_GPCHR },
+        _cfgl   { ((PORTA_Type *)cfgbase)->PORTA_GPCLR },
+        _mask   { 1U << index }
+    {}
+
+protected:
+    static const uint8_t    func_analog = 0;
+    static const uint8_t    func_gpio = 1;
+
+    __IOM uint32_t  &_cfgh;
+    __IOM uint32_t  &_cfgl;
+    const uint32_t  _mask;
+
+    void configure(uint8_t function) volatile {
+        if (_mask < 0x10000) {
+            _cfgl = (_mask << 16) | (function << 8);
+        } else {
+            _cfgh = _mask | (function << 8);
+        }
+    }
+};
+
+class _GPIO : public _Pin
+{
+public:
+    constexpr _GPIO(__IO void *iobase,
+                    __IO void *cfgbase,
+                    uint8_t index) :
+        _Pin(cfgbase, index),
+        _set        { ((PTA_Type *)iobase)->GPIOA_PSOR },
+        _clear      { ((PTA_Type *)iobase)->GPIOA_PCOR },
+        _toggle     { ((PTA_Type *)iobase)->GPIOA_PTOR },
+        _data       { ((PTA_Type *)iobase)->GPIOA_PDOR },
+        _direction  { ((PTA_Type *)iobase)->GPIOA_PDDR }
+    {}
+
+protected:
+    static const bool           mode_output = true;
+    static const bool           mode_input = false;
+
+    __IOM uint32_t  &_set;
+    __IOM uint32_t  &_clear;
+    __IOM uint32_t  &_toggle;
+    __IOM uint32_t  &_data;
+    __IOM uint32_t  &_direction;
+
+    void configure(bool mode)
+    {
+        if (mode == mode_output) {
+            _direction |= _mask;
+        } else {
+            _direction &= ~_mask;
+        }
+        _Pin::configure(func_gpio); 
+    }
+};
+
+class GPIOOut : public _GPIO
+{
+public:
+    constexpr GPIOOut(__IO void *iobase,
+                      __IO void *cfgbase,
+                      uint8_t index,
+                      bool initial=false) :
+        _GPIO(iobase, cfgbase, index),
+        _initial { initial }
+    {}
+
+    void set(void)              volatile { _set = _mask; }
+    void set(bool v)            volatile { if (v) set(); else clear(); }
+    void clear(void)            volatile { _clear = _mask; }
+    void toggle(void)           volatile { _toggle = _mask; }
+    bool get(void)              volatile { return _data & _mask; }
+    uint32_t mask(void)         volatile { return _mask; }
+
+    void operator = (bool v)    volatile { set(v); }
+    bool operator()(void)       volatile { return get(); }
+
+    void configure()
+    {
+        set(_initial);
+        _GPIO::configure(mode_output);
+    }
+
+private:
+    const bool      _initial;
+};
+
+class GPIOIn : public _GPIO
+{
+public:
+    constexpr GPIOIn(__IO void *iobase,
+                     __IO void *cfgbase,
+                     uint8_t index) :
+        _GPIO(iobase, cfgbase, index)
+    {}
+
+    bool get(void)              volatile { return _data & _mask; }
+    bool operator()(void)       volatile { return get(); }
+    void configure()            { _GPIO::configure(mode_input); }
+};
+
+class AnalogPin : public _Pin
+{
+public:
+    constexpr AnalogPin(__IO void *cfgbase,
+                        uint8_t index) :
+        _Pin(cfgbase, index)
+    {}
+
+    void configure(void)        { _Pin::configure(func_analog); }
+};
+
+class PeripheralPin : public _Pin
+{
+public:
+    constexpr PeripheralPin(__IO void *cfgbase,
+                            uint8_t index,
+                            uint8_t function) :
+        _Pin(cfgbase, index),
+        _function               { function }
+    {}
+
+    void configure(void)        { _Pin::configure(_function); }
+
+private:
+    const uint8_t   _function;
+};
+
+//
+// Pin indices in port space
+//
+typedef enum
+{
+    _DI_AI_OUT1,
+    _DI_AI_OUT4,
+    _DI_AI_OUT6,
+    _DI_AI_OUT7,
+    _MC_JTAG_TMS,
+    _MC_JTAG_TRST,
+    _DI_AI_A_IN3,
+    _DI_AI_A_IN2,
+    _MC_SCI_RXD,
+    _MC_SCI_TXD,
+    _MC_JTAG_TDO,
+    _CAN_EN1,
+    _MC_CAN_RXD2,
+    _MC_CAN_TXD2,
+    _CAN_WAKE1,
+    _DI_AI_OUT2,
+    _DI_AI_OUT3,
+    _DI_PGD,
+} PortA;
+
+typedef enum 
+{
+    _DI_AI_A_IN1,
+    _DI_AI_A_IN0,
+    _DI_AI_INA_OUT4,
+    _DI_AI_INA_OUT7,
+    _DO_HSD1_OUT1,
+    _DO_HSD1_OUT3,
+    _XTAL,
+    _EXTAL,
+    _DO_RS2,
+    _MC_FREQ_A_IN4,
+    _MC_FREQ_A_IN5,
+    _DO_RS4,
+    _DI_AI_SNS4,
+    _DI_AI_SNS1,
+    _DI_AI_SNS2,
+    _DI_AI_A_IN5,
+    _DI_AI_A_IN4,
+    _DO_RS5,
+} PortB;
+
+typedef enum 
+{
+    _DI_AI_INA_OUT3,
+    _DI_AI_INA_OUT2,
+    _DI_AI_KL30_1,
+    _DI_AI_KL30_2,
+    _MC_JTAG_TCLK,
+    _MC_JTAG_TDI,
+    _DI_AI_VARIANTE,
+    _DI_AI_ID,
+    _DO_RS3,
+    _DO_RS0,
+    _MC_FREQ_A_IN0,
+    _MC_FREQ_A_IN1,
+    _MC_FREQ_A_IN2,
+    _MC_FREQ_A_IN3,
+    _DI_AI_INA_OUT5,
+    _DI_AI_INA_OUT6,
+    _DI_AI_INA_OUT0,
+    _DI_AI_INA_OUT1,
+} PortC;
+
+typedef enum 
+{
+    _DO_SHIFT_SH_CP,
+    _DO_SHIFT_ST_CP,
+    _DI_AI_OUT5,
+    _DI_AI_VREF,
+    _DI_AI_SNS3,
+    _DO_HSD2_OUT4,
+    _DOX_SHIFT_IN_DS,
+    _DI_INTERFACE2_A,
+    _DI_INTERFACE2_B,
+    _DO_HSD2_OUT6,
+    _DO_VREF_EN,
+    _DO_CS_HSD2,
+    _DO_HSD2_OUT5,
+    _DI_KL15,
+    _DO_HSD1_OUT0,
+    _DO_POWER,
+    _DO_HSD2_OUT7,
+    _DO_CS_HSD1,
+} PortD;
+
+typedef enum
+{
+    _CAN_ERR1,
+    _CAN_STB1,
+    _Pin74,
+    _CAN_STB2,
+    _MC_CAN_RXD1,
+    _MC_CAN_TXD1,
+    _DI_AI_OUT0,
+    _DO_RS1,
+    _DO_HSD1_OUT2,
+    _LIN_EN,
+    _Pin82,
+    _WD,
+    _CAN_EN2,
+    _CAN_ERR2,
+    _CAN_WAKE2,
+    _DO_SHIFT_OE,
+    _DO_SHIFT_MR,
+    _PIN_MAX,
+} PortE;
+
+//
+// GPIO pins.
+//
+#define CAN_EN1         ::Pin::GPIOOut(PTA, PORTA, ::Pin::PortA::_CAN_EN1, 1)
+#define CAN_EN2         ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_CAN_EN2, 1)
+#define CAN_ERR1        ::Pin::GPIOIn(PTE, PORTE, ::Pin::PortE::_CAN_ERR1)
+#define CAN_ERR2        ::Pin::GPIOIn(PTE, PORTE, ::Pin::PortE::_CAN_ERR2)
+#define CAN_STB1        ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_CAN_STB1, 1)
+#define CAN_STB2        ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_CAN_STB2, 1)
+#define CAN_WAKE1       ::Pin::GPIOOut(PTA, PORTA, ::Pin::PortA::_CAN_WAKE1, 1)
+#define CAN_WAKE2       ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_CAN_WAKE2, 1)
+#define DI_INTERFACE2_A ::Pin::GPIOIn(PTD, PORTD, ::Pin::PortD::_DI_INTERFACE2_A)
+#define DI_INTERFACE2_B ::Pin::GPIOIn(PTD, PORTD, ::Pin::PortD::_DI_INTERFACE2_B)
+#define DI_KL15         ::Pin::GPIOIn(PTD, PORTD, ::Pin::PortD::_DI_KL15)
+#define DI_PGD          ::Pin::GPIOIn(PTA, PORTA, ::Pin::PortA::_DI_PGD)
+#define DO_CS_HSD1      ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_CS_HSD1)
+#define DO_CS_HSD2      ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_CS_HSD2)
+#define DO_POWER        ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_POWER, 1)
+#define DO_RS0          ::Pin::GPIOOut(PTC, PORTC, ::Pin::PortC::_DO_RS0)
+#define DO_RS1          ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_DO_RS1)
+#define DO_RS2          ::Pin::GPIOOut(PTB, PORTB, ::Pin::PortB::_DO_RS2)
+#define DO_RS3          ::Pin::GPIOOut(PTC, PORTC, ::Pin::PortC::_DO_RS3)
+#define DO_RS4          ::Pin::GPIOOut(PTB, PORTB, ::Pin::PortB::_DO_RS4)
+#define DO_RS5          ::Pin::GPIOOut(PTB, PORTB, ::Pin::PortB::_DO_RS5)
+#define DO_SHIFT_MR     ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_DO_SHIFT_MR)
+#define DO_SHIFT_OE     ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_DO_SHIFT_OE)
+#define DO_SHIFT_SH_CP  ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_SHIFT_SH_CP)
+#define DO_SHIFT_ST_CP  ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_SHIFT_ST_CP)
+#define DO_VREF_EN      ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DO_VREF_EN)
+#define DOX_SHIFT_IN_DS ::Pin::GPIOOut(PTD, PORTD, ::Pin::PortD::_DOX_SHIFT_IN_DS)
+#define LIN_EN          ::Pin::GPIOOut(PTE, PORTE, ::Pin::PortE::_LIN_EN)
+
+//
+// Peripheral pins not requiring other setup.
+//
+#define MC_CAN_RXD1     ::Pin::PeripheralPin(PORTE, ::Pin::PortE::_MC_CAN_RXD1, 5)
+#define MC_CAN_RXD2     ::Pin::PeripheralPin(PORTA, ::Pin::PortA::_MC_CAN_RXD2, 3)
+#define MC_CAN_TXD1     ::Pin::PeripheralPin(PORTE, ::Pin::PortE::_MC_CAN_TXD1, 5)
+#define MC_CAN_TXD2     ::Pin::PeripheralPin(PORTA, ::Pin::PortA::_MC_CAN_TXD2, 3)
+#define MC_SCI_RXD      ::Pin::PeripheralPin(PORTA, ::Pin::PortA::_MC_SCI_RXD, 2)
+#define MC_SCI_TXD      ::Pin::PeripheralPin(PORTA, ::Pin::PortA::_MC_SCI_TXD, 2)
+#define Pin74           ::Pin::PeripheralPin(PORTE, ::Pin::PortE::_Pin74, 0)
+#define Pin82           ::Pin::PeripheralPin(PORTE, ::Pin::PortE::_Pin82, 0)
+#define WD              ::Pin::PeripheralPin(PORTE, ::Pin::PortE::_WD, 0)
+
+
+#define MC_FREQ_A_IN0   ::Pin::PeripheralPin(PORTC, ::Pin::PortC::_MC_FREQ_A_IN0, 2)
+#define MC_FREQ_A_IN1   ::Pin::PeripheralPin(PORTC, ::Pin::PortC::_MC_FREQ_A_IN1, 2)
+#define MC_FREQ_A_IN2   ::Pin::PeripheralPin(PORTC, ::Pin::PortC::_MC_FREQ_A_IN2, 2)
+#define MC_FREQ_A_IN3   ::Pin::PeripheralPin(PORTC, ::Pin::PortC::_MC_FREQ_A_IN3, 2)
+#define MC_FREQ_A_IN4   ::Pin::PeripheralPin(PORTB, ::Pin::PortB::_MC_FREQ_A_IN4, 2)
+#define MC_FREQ_A_IN5   ::Pin::PeripheralPin(PORTB, ::Pin::PortB::_MC_FREQ_A_IN5, 2)
+
+#define DI_AI_A_IN0     ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_A_IN0)
+#define DI_AI_A_IN1     ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_A_IN1)
+#define DI_AI_A_IN2     ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_A_IN2)
+#define DI_AI_A_IN3     ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_A_IN3)
+#define DI_AI_A_IN4     ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_A_IN4)
+#define DI_AI_A_IN5     ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_A_IN5)
+#define DI_AI_ID        ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_ID)
+#define DI_AI_INA_OUT0  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT0)
+#define DI_AI_INA_OUT1  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT1)
+#define DI_AI_INA_OUT2  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT2)
+#define DI_AI_INA_OUT3  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT3)
+#define DI_AI_INA_OUT4  ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_INA_OUT4)
+#define DI_AI_INA_OUT5  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT5)
+#define DI_AI_INA_OUT6  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_INA_OUT6)
+#define DI_AI_INA_OUT7  ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_INA_OUT7)
+#define DI_AI_KL30_1    ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_KL30_1)
+#define DI_AI_KL30_2    ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_KL30_2)
+#define DI_AI_OUT0      ::Pin::AnalogPin(PORTE, ::Pin::PortE::_DI_AI_OUT0)
+#define DI_AI_OUT1      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT1)
+#define DI_AI_OUT2      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT2)
+#define DI_AI_OUT3      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT3)
+#define DI_AI_OUT4      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT4)
+#define DI_AI_OUT5      ::Pin::AnalogPin(PORTD, ::Pin::PortD::_DI_AI_OUT5)
+#define DI_AI_OUT6      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT6)
+#define DI_AI_OUT7      ::Pin::AnalogPin(PORTA, ::Pin::PortA::_DI_AI_OUT7)
+#define DI_AI_SNS1      ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_SNS1)
+#define DI_AI_SNS2      ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_SNS2)
+#define DI_AI_SNS3      ::Pin::AnalogPin(PORTD, ::Pin::PortD::_DI_AI_SNS3)
+#define DI_AI_SNS4      ::Pin::AnalogPin(PORTB, ::Pin::PortB::_DI_AI_SNS4)
+#define DI_AI_VARIANTE  ::Pin::AnalogPin(PORTC, ::Pin::PortC::_DI_AI_VARIANTE)
+#define DI_AI_VREF      ::Pin::AnalogPin(PORTD, ::Pin::PortD::_DI_AI_VREF)
+
+} // namespace GPIO
--- libs/CC16/start.cpp
+++ libs/CC16/start.cpp
@@ -4,7 +4,8 @@
 
 #include <CMSIS/S32K144.h>
 
-#include "_pin_names.h"
+#include "pin.h"
+#include "pwm.h"
 
 extern "C" void main(void);
 
@@ -16,6 +17,7 @@ extern "C" void app_loop(void) __attribute__((weak, alias("_app_no_loop")));
 
 static void clock_setup(void);
 static void pin_setup(void);
+static void pwm_setup(void);
 
 void
 main(void)
@@ -23,7 +25,7 @@ main(void)
     /* do system hardware init */
     clock_setup();
     pin_setup();
-
+    pwm_setup();
     // CAN
     // ADC
     // PWM
@@ -47,250 +49,110 @@ clock_setup(void)
     // The bootloader has already configured the PLL and the 
     // clock tree, so we can leave it alone.
 
-    //
-    // Turn on missing peripheral clocks.
-    //
-    PCC->PCC_ADC0_b.CGC = 1;
-    PCC->PCC_ADC1_b.CGC = 1;
-    PCC->PCC_FlexCAN1_b.CGC = 1;
+    // GPIO clocks on
     PCC->PCC_PORTB_b.CGC = 1;
     PCC->PCC_PORTC_b.CGC = 1;
     PCC->PCC_PORTD_b.CGC = 1;
+
+    // FTM0-2 on, running from SYS_CLK
+    PCC->PCC_FTM0_b.PCS = 1;
+    PCC->PCC_FTM0_b.CGC = 1;
+    PCC->PCC_FTM1_b.PCS = 1;
+    PCC->PCC_FTM1_b.CGC = 1;
+    PCC->PCC_FTM2_b.PCS = 1;
+    PCC->PCC_FTM2_b.CGC = 1;
 }
 
 void
 pin_setup(void)
 {
-    //
-    // bulk-configure analog input pins
-    // mux disable, no pulls
-    //
-#define PORTA_ANALOG_PINS    \
-    ((1U << DI_AI_OUT1)     |\
-     (1U << DI_AI_OUT4)     |\
-     (1U << DI_AI_OUT6)     |\
-     (1U << DI_AI_OUT7)     |\
-     (1U << DI_AI_A_IN3)    |\
-     (1U << DI_AI_A_IN2)    |\
-     (1U << DI_AI_OUT2)     |\
-     (1U << DI_AI_OUT3))
+    CAN_EN1.configure();
+    CAN_EN2.configure();
+    CAN_ERR1.configure();
+    CAN_ERR2.configure();
+    CAN_STB1.configure();
+    CAN_STB2.configure();
+    CAN_WAKE1.configure();
+    CAN_WAKE2.configure();
+    DI_AI_A_IN0.configure();
+    DI_AI_A_IN1.configure();
+    DI_AI_A_IN2.configure();
+    DI_AI_A_IN3.configure();
+    DI_AI_A_IN4.configure();
+    DI_AI_A_IN5.configure();
+    DI_AI_ID.configure();
+    DI_AI_INA_OUT0.configure();
+    DI_AI_INA_OUT1.configure();
+    DI_AI_INA_OUT2.configure();
+    DI_AI_INA_OUT3.configure();
+    DI_AI_INA_OUT4.configure();
+    DI_AI_INA_OUT5.configure();
+    DI_AI_INA_OUT6.configure();
+    DI_AI_INA_OUT7.configure();
+    DI_AI_KL30_1.configure();
+    DI_AI_KL30_2.configure();
+    DI_AI_OUT0.configure();
+    DI_AI_OUT1.configure();
+    DI_AI_OUT2.configure();
+    DI_AI_OUT3.configure();
+    DI_AI_OUT4.configure();
+    DI_AI_OUT5.configure();
+    DI_AI_OUT6.configure();
+    DI_AI_OUT7.configure();
+    DI_AI_SNS1.configure();
+    DI_AI_SNS2.configure();
+    DI_AI_SNS3.configure();
+    DI_AI_SNS4.configure();
+    DI_AI_VARIANTE.configure();
+    DI_AI_VREF.configure();
+    DI_INTERFACE2_A.configure();
+    DI_INTERFACE2_B.configure();
+    DI_KL15.configure();
+    DI_PGD.configure();
+    DO_CS_HSD1.configure();
+    DO_CS_HSD2.configure();
+    DO_HSD1_OUT0.configure();
+    DO_HSD1_OUT1.configure();
+    DO_HSD1_OUT2.configure();
+    DO_HSD1_OUT3.configure();
+    DO_HSD2_OUT4.configure();
+    DO_HSD2_OUT5.configure();
+    DO_HSD2_OUT6.configure();
+    DO_HSD2_OUT7.configure();
+    DO_POWER.configure();
+    DO_RS0.configure();
+    DO_RS1.configure();
+    DO_RS2.configure();
+    DO_RS3.configure();
+    DO_RS4.configure();
+    DO_RS5.configure();
+    DO_SHIFT_MR.configure();
+    DO_SHIFT_OE.configure();
+    DO_SHIFT_SH_CP.configure();
+    DO_SHIFT_ST_CP.configure();
+    DO_VREF_EN.configure();
+    DOX_SHIFT_IN_DS.configure();
+    LIN_EN.configure();
+    MC_CAN_RXD1.configure();
+    MC_CAN_RXD2.configure();
+    MC_CAN_TXD1.configure();
+    MC_CAN_TXD2.configure();
+    MC_FREQ_A_IN0.configure();
+    MC_FREQ_A_IN1.configure();
+    MC_FREQ_A_IN2.configure();
+    MC_FREQ_A_IN3.configure();
+    MC_FREQ_A_IN4.configure();
+    MC_FREQ_A_IN5.configure();
+    MC_SCI_RXD.configure();
+    MC_SCI_TXD.configure();
+    Pin74.configure();
+    Pin82.configure();
+    WD.configure();
+}
 
-    PORTA->PORTA_GPCLR = ((PORTA_ANALOG_PINS & 0xFFFF) << 16) | (0U << 8);
-    PORTA->PORTA_GPCHR = ((PORTA_ANALOG_PINS & 0xFFFF0000))   | (0U << 8);
 
-#define PORTB_ANALOG_PINS    \
-    ((1U << DI_AI_A_IN1)    |\
-     (1U << DI_AI_A_IN0)    |\
-     (1U << DI_AI_INA_OUT4) |\
-     (1U << DI_AI_INA_OUT7) |\
-     (1U << DI_AI_SNS4)     |\
-     (1U << DI_AI_SNS1)     |\
-     (1U << DI_AI_SNS2)     |\
-     (1U << DI_AI_A_IN5)    |\
-     (1U << DI_AI_A_IN4))
-
-    PORTB->PORTB_GPCLR = ((PORTB_ANALOG_PINS & 0xFFFF) << 16) | (0U << 8);
-    PORTB->PORTB_GPCHR = ((PORTB_ANALOG_PINS & 0xFFFF0000))   | (0U << 8);
-
-#define PORTC_ANALOG_PINS    \
-    ((1U << DI_AI_INA_OUT3) |\
-     (1U << DI_AI_INA_OUT2) |\
-     (1U << DI_AI_KL30_1)   |\
-     (1U << DI_AI_KL30_2)   |\
-     (1U << DI_AI_VARIANTE) |\
-     (1U << DI_AI_ID)       |\
-     (1U << DI_AI_INA_OUT5) |\
-     (1U << DI_AI_INA_OUT6) |\
-     (1U << DI_AI_INA_OUT0) |\
-     (1U << DI_AI_INA_OUT1))
-
-    PORTC->PORTC_GPCLR = ((PORTC_ANALOG_PINS & 0xFFFF) << 16) | (0U << 8);
-    PORTC->PORTC_GPCHR = ((PORTC_ANALOG_PINS & 0xFFFF0000))   | (0U << 8);
-
-#define PORTD_ANALOG_PINS    \
-    ((1U << DI_AI_OUT5)     |\
-     (1U << DI_AI_VREF)     |\
-     (1U << DI_AI_SNS3))
-
-    PORTD->PORTD_GPCLR = ((PORTD_ANALOG_PINS & 0xFFFF) << 16) | (0U << 8);
-    PORTD->PORTD_GPCHR = ((PORTD_ANALOG_PINS & 0xFFFF0000))   | (0U << 8);
-
-#define PORTE_ANALOG_PINS    \
-    ((1U << Pin74)          |\
-     (1U << DI_AI_OUT0)     |\
-     (1U << Pin82)          |\
-     (1U << WD))
-
-    PORTE->PORTE_GPCLR = ((PORTE_ANALOG_PINS & 0xFFFF) << 16) | (0U << 8);
-    PORTE->PORTE_GPCHR = ((PORTE_ANALOG_PINS & 0xFFFF0000))   | (0U << 8);
-
-    //
-    // bulk-configure special functions
-    //
-#define PORTA_ALT2_PINS      \
-    ((1U << MC_SCI_RXD)     |\
-     (1U << MC_SCI_TXD))
-#define PORTA_ALT3_PINS      \
-    ((1U << MC_CAN_RXD2)    |\
-     (1U << MC_CAN_TXD2))
-
-    PORTA->PORTA_GPCLR = ((PORTA_ALT2_PINS & 0xFFFF) << 16) | (2U << 8);
-    PORTA->PORTA_GPCHR = ((PORTA_ALT2_PINS & 0xFFFF0000))   | (2U << 8);
-    PORTA->PORTA_GPCLR = ((PORTA_ALT3_PINS & 0xFFFF) << 16) | (3U << 8);
-    PORTA->PORTA_GPCHR = ((PORTA_ALT3_PINS & 0xFFFF0000))   | (3U << 8);
-
-#define PORTB_ALT2_PINS      \
-    ((1U << DO_HSD1_OUT1)   |\
-     (1U << DO_HSD1_OUT3)   |\
-     (1U << MC_FREQ_A_IN4)  |\
-     (1U << MC_FREQ_A_IN5))
-
-    PORTB->PORTB_GPCLR = ((PORTB_ALT2_PINS & 0xFFFF) << 16) | (2U << 8);
-    PORTB->PORTB_GPCHR = ((PORTB_ALT2_PINS & 0xFFFF0000))   | (2U << 8);
-
-#define PORTC_ALT2_PINS      \
-    ((1U << MC_FREQ_A_IN0)  |\
-     (1U << MC_FREQ_A_IN1)  |\
-     (1U << MC_FREQ_A_IN2)  |\
-     (1U << MC_FREQ_A_IN3))
-
-    PORTC->PORTC_GPCLR = ((PORTC_ALT2_PINS & 0xFFFF) << 16) | (2U << 8);
-    PORTC->PORTC_GPCHR = ((PORTC_ALT2_PINS & 0xFFFF0000))   | (2U << 8);
-
-#define PORTD_ALT2_PINS      \
-    ((1U << DO_HSD2_OUT4)   |\
-     (1U << DO_HSD2_OUT5)   |\
-     (1U << DO_HSD1_OUT0)   |\
-     (1U << DO_HSD2_OUT7))
-#define PORTD_ALT6_PINS      \
-    (1U << DO_HSD2_OUT6)
-
-    PORTD->PORTD_GPCLR = ((PORTD_ALT2_PINS & 0xFFFF) << 16) | (2U << 8);
-    PORTD->PORTD_GPCHR = ((PORTD_ALT2_PINS & 0xFFFF0000))   | (2U << 8);
-    PORTD->PORTD_GPCLR = ((PORTD_ALT6_PINS & 0xFFFF) << 16) | (6U << 8);
-    PORTD->PORTD_GPCHR = ((PORTD_ALT6_PINS & 0xFFFF0000))   | (6U << 8);
-
-#define PORTE_ALT2_PINS      \
-    (1U << DO_HSD1_OUT2)
-#define PORTE_ALT5_PINS      \
-    ((1U << MC_CAN_RXD1)    |\
-     (1U << MC_CAN_TXD1))
-
-    PORTE->PORTE_GPCLR = ((PORTE_ALT2_PINS & 0xFFFF) << 16) | (2U << 8);
-    PORTE->PORTE_GPCHR = ((PORTE_ALT2_PINS & 0xFFFF0000))   | (2U << 8);
-    PORTE->PORTE_GPCLR = ((PORTE_ALT5_PINS & 0xFFFF) << 16) | (5U << 8);
-    PORTE->PORTE_GPCHR = ((PORTE_ALT5_PINS & 0xFFFF0000))   | (5U << 8);
-
-    //
-    // bulk-configure GPIOs
-    // all have low drive strength, no pulls
-    //
-
-#define PORTA_GPIO_PINS      \
-    ((1U << CAN_EN1)        |\
-     (1U << CAN_WAKE1)      |\
-     (1U << DI_PGD))
-#define PORTA_GPIO_OUTPUTS   \
-    ((1U << CAN_EN1)        |\
-     (1U << CAN_WAKE1))
-#define PORTA_GPIO_DEFAULTS  \
-    ((1U << CAN_EN1)        |\
-     (1U << CAN_WAKE1))
-
-    PORTA->PORTA_GPCLR = ((PORTA_GPIO_PINS & 0xFFFF) << 16) | (1U << 8);
-    PORTA->PORTA_GPCHR = ((PORTA_GPIO_PINS & 0xFFFF0000))   | (1U << 8);
-    PTA->GPIOA_PDOR = PORTA_GPIO_DEFAULTS;
-    PTA->GPIOA_PDDR = PORTA_GPIO_OUTPUTS;
-
-#define PORTB_GPIO_PINS      \
-    ((1U << DO_RS2)         |\
-     (1U << DO_RS4)         |\
-     (1U << DO_RS5))
-#define PORTB_GPIO_OUTPUTS   \
-    ((1U << DO_RS2)         |\
-     (1U << DO_RS4)         |\
-     (1U << DO_RS5))
-#define PORTB_GPIO_DEFAULTS  \
-    ((1U << DO_RS2)         |\
-     (1U << DO_RS4)         |\
-     (1U << DO_RS5))
-
-    PORTB->PORTB_GPCLR = ((PORTB_GPIO_PINS & 0xFFFF) << 16) | (1U << 8);
-    PORTB->PORTB_GPCHR = ((PORTB_GPIO_PINS & 0xFFFF0000))   | (1U << 8);
-    PTB->GPIOB_PDOR = PORTB_GPIO_DEFAULTS;
-    PTB->GPIOB_PDDR = PORTB_GPIO_OUTPUTS;
-
-#define PORTC_GPIO_PINS      \
-    ((1U << DO_RS3)         |\
-     (1U << DO_RS0))
-#define PORTC_GPIO_OUTPUTS   \
-    ((1U << DO_RS3)         |\
-     (1U << DO_RS0))
-#define PORTC_GPIO_DEFAULTS  \
-    ((1U << DO_RS3)         |\
-     (1U << DO_RS0))
-
-    PORTC->PORTC_GPCLR = ((PORTC_GPIO_PINS & 0xFFFF) << 16) | (1U << 8);
-    PORTC->PORTC_GPCHR = ((PORTC_GPIO_PINS & 0xFFFF0000))   | (1U << 8);
-    PTC->GPIOC_PDOR = PORTC_GPIO_DEFAULTS;
-    PTC->GPIOC_PDDR = PORTC_GPIO_OUTPUTS;
-
-#define PORTD_GPIO_PINS      \
-    ((1U << DO_SHIFT_SH_CP) |\
-     (1U << DO_SHIFT_ST_CP) |\
-     (1U << DOX_SHIFT_IN_DS)|\
-     (1U << DI_INTERFACE2_A)|\
-     (1U << DI_INTERFACE2_B)|\
-     (1U << DO_VREF_EN)     |\
-     (1U << DO_CS_HSD2)     |\
-     (1U << DI_KL15)        |\
-     (1U << DO_POWER)       |\
-     (1U << DO_CS_HSD1))
-#define PORTD_GPIO_OUTPUTS   \
-    ((1U << DO_SHIFT_SH_CP) |\
-     (1U << DO_SHIFT_ST_CP) |\
-     (1U << DOX_SHIFT_IN_DS)|\
-     (1U << DO_VREF_EN)     |\
-     (1U << DO_CS_HSD2)     |\
-     (1U << DO_POWER)       |\
-     (1U << DO_CS_HSD1))
-#define PORTD_GPIO_DEFAULTS  \
-    ((1U << DO_POWER))
-
-    PORTD->PORTD_GPCLR = ((PORTD_GPIO_PINS & 0xFFFF) << 16) | (1U << 8);
-    PORTD->PORTD_GPCHR = ((PORTD_GPIO_PINS & 0xFFFF0000))   | (1U << 8);
-    PTD->GPIOD_PDOR = PORTD_GPIO_DEFAULTS;
-    PTD->GPIOD_PDDR = PORTD_GPIO_OUTPUTS;
-
-#define PORTE_GPIO_PINS      \
-    ((1U << CAN_ERR1)       |\
-     (1U << CAN_STB1)       |\
-     (1U << CAN_STB2)       |\
-     (1U << DO_RS1)         |\
-     (1U << LIN_EN)         |\
-     (1U << CAN_EN2)        |\
-     (1U << CAN_ERR2)       |\
-     (1U << CAN_WAKE2)      |\
-     (1U << DO_SHIFT_OE)    |\
-     (1U << DO_SHIFT_MR))
-#define PORTE_GPIO_OUTPUTS   \
-    ((1U << CAN_STB1)       |\
-     (1U << CAN_STB2)       |\
-     (1U << DO_RS1)         |\
-     (1U << LIN_EN)         |\
-     (1U << CAN_EN2)        |\
-     (1U << CAN_WAKE2)      |\
-     (1U << DO_SHIFT_OE)    |\
-     (1U << DO_SHIFT_MR))
-#define PORTE_GPIO_DEFAULTS  \
-    ((1U << CAN_STB1)       |\
-     (1U << CAN_STB2)       |\
-     (1U << DO_RS1)         |\
-     (1U << CAN_EN2)        |\
-     (1U << CAN_WAKE2))
-
-    PORTE->PORTE_GPCLR = ((PORTE_GPIO_PINS & 0xFFFF) << 16) | (1U << 8);
-    PORTE->PORTE_GPCHR = ((PORTE_GPIO_PINS & 0xFFFF0000))   | (1U << 8);
-    PTE->GPIOE_PDOR = PORTE_GPIO_DEFAULTS;
-    PTE->GPIOE_PDDR = PORTE_GPIO_OUTPUTS;
+void
+pwm_setup(void)
+{
+    // XXX
 }
