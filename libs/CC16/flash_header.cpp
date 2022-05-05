c64becb746f92433418a93c2bbeee2c4cfb69984
none <user@example.com>
2022-05-05 12:03:19 -0700

    Pad out various heading sections to make sure the S-records cover these spaces.

--- libs/CC16/flash_header.cpp
+++ libs/CC16/flash_header.cpp
@@ -16,5 +16,11 @@ static const struct
 } flash_header __attribute__((used, section(".application_header"))) = { 
     .header_key = 0x12345678,
     .app_header_version = 1,
-    .application_length = (uint32_t)&__application_size,
+};
+
+static const uint32_t flash_configuration[] __attribute__((used, section(".flash_config"))) = {
+    0xffffffff,    // backdoor comparison key 1/2
+    0xffffffff,    // backdoor comparison key 2/2
+    0xffffffff,    // program flash protection bytes
+    0xffff7ffe,    // FDPROT:FEPROT:FOPT:FSEC(0xFE = unsecured)
 };
